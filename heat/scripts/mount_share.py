a9204d25e91c569e7a70caf0cfaa9f5f090fecbb
Stéphane Gerard <stgerard@vub.ac.be>
2019-03-13 15:45:19 +0100

    import splitted on several lines to follow PEP style guidelines

--- heat/scripts/mount_share.py
+++ heat/scripts/mount_share.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
-import os,json,re,requests
+import os
+import json
+import re
+import requests
 r = requests.get('http://169.254.169.254/openstack/latest/meta_data.json')
 data = r.json()
 m = re.search('\'(.+?)\'', data['meta']['share_export_loc_path'])
