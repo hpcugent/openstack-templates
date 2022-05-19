775b0d0878bbfa5858386e6fc1ace5b7aacdf5aa
root <hardy@hardynet.cz>
2022-05-19 10:35:44 +0200

    changing run_ansible.sh to pass shellchecker

--- heat/scripts/run_ansible.sh
+++ heat/scripts/run_ansible.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
-OS="$(cat /etc/os-release |grep ^NAME=|tr [:upper:] [:lower:])"
-[[ $OS =~ "fedora" || $OS =~ "red hat enterprise linux" || $OS =~ "centos" || $OS =~ "rocky" || $OS =~ "alma" ]] && yum -y install epel-release && yum -y install ansible curl
+OS="$(grep ^NAME= /etc/os-release |tr \"[:upper:]\" \"[:lower:]\")"
+[[ $OS =~ "red hat enterprise linux" || $OS =~ "centos" || $OS =~ "rocky" || $OS =~ "alma" ]] && yum -y install epel-release && yum -y install ansible curl
 [[ $OS =~ "debian" || $OS =~ "ubuntu" ]] && apt-get update && apt-get -y install ansible curl
 curl -L --connect-timeout 60 -o /tmp/my_playbook.yaml "__ANSIBLE_URL__"
 ansible-playbook /tmp/my_playbook.yaml
-[ $? -eq 0 ] && wc_notify --data-binary "{\"status\": \"SUCCESS\", \"reason\": \"Ansible playbook successful.\"}" || wc_notify --data-binary "{\"status\": \"FAILURE\", \"reason\": \"Ansible playbook not successful.\"}"
+retcode=$?
+if [ $retcode -eq 0 ]
+then
+	wc_notify --data-binary "{\"status\": \"SUCCESS\", \"reason\": \"Ansible playbook successful.\"}" 
+else
+        wc_notify --data-binary "{\"status\": \"FAILURE\", \"reason\": \"Ansible playbook not successful.\"}"
+fi
