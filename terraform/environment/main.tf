1392441542f0a3b91d103566e7f5b0e514e33097
root <hardy@hardynet.cz>
2022-06-15 10:03:09 +0200

    adding script which generates terraform variables using openstack client

--- terraform/environment/main.tf
+++ terraform/environment/main.tf
@@ -2,27 +2,27 @@ module "vm_with_pf_rules_with_ssh_access" {
   source   = "../modules/vm_with_pf_rules_with_ssh_access"
 
   vm_name              = "MyVM"
-  floating_ip_id       = "dd8b33b1-d178-4229-9df6-00157c61181c"
-  vm_network_id        = "985df437-2555-471d-9836-48355e6e7e9b"
-  vm_subnet_id         = "05f58ec4-5b6c-466a-9f18-78aac2ccf9f6"
-  access_key           = "ssh-rsa AAAAB3Nz_Lxyy1Q__ root_charon"
+  floating_ip_id       = "_FLOATING_IP_ID_"
+  vm_network_id        = "_VM_NETWORK_ID_"
+  vm_subnet_id         = "_VM_SUBNET_ID_"
+  access_key           = "_ACCESS_KEY_"
   image_name           = "CentOS-8-stream"
   flavor_name          = "CPUv1.small"
-  ssh_forwarded_port   = "52201"
+  ssh_forwarded_port   = "_SSH_FORWARDED_PORT1_"
 }
 
 module "vm_with_pf_rules_with_ssh_access_with_nginx" {
   source   = "../modules/vm_with_pf_rules_with_ssh_access_with_nginx"
 
   vm_name              = "MyVM"
-  floating_ip_id       = "dd8b33b1-d178-4229-9df6-00157c61181c"
-  vm_network_id        = "985df437-2555-471d-9836-48355e6e7e9b"
-  vm_subnet_id         = "05f58ec4-5b6c-466a-9f18-78aac2ccf9f6"
-  access_key           = "ssh-rsa AAAAB3Nz_Lxyy1Q__ root_charon"
+  floating_ip_id       = "_FLOATING_IP_ID_"
+  vm_network_id        = "_VM_NETWORK_ID_"
+  vm_subnet_id         = "_VM_SUBNET_ID_"
+  access_key           = "_ACCESS_KEY_"
   image_name           = "CentOS-8-stream"
   flavor_name          = "CPUv1.small"
-  ssh_forwarded_port   = "52202"
-  http_forwarded_port  = "8088"
+  ssh_forwarded_port   = "_SSH_FORWARDED_PORT2_"
+  http_forwarded_port  = "_HTTP_FORWARDED_PORT_"
   ansible_playbook_url = "https://raw.githubusercontent.com/hpcugent/openstack-templates/master/heat/playbooks/install_nginx.yaml"
 }
 
@@ -30,32 +30,31 @@ module "vm_with_pf_rules_with_ssh_access_with_vsc_net" {
   source   = "../modules/vm_with_pf_rules_with_ssh_access_with_vsc_net"
 
   vm_name              = "MyVM"
-  floating_ip_id       = "dd8b33b1-d178-4229-9df6-00157c61181c"
-  vsc_floating_ip      = "172.24.48.5"
-  vm_network_id        = "985df437-2555-471d-9836-48355e6e7e9b"
-  vm_subnet_id         = "05f58ec4-5b6c-466a-9f18-78aac2ccf9f6"
-  vsc_network_id       = "fc52017a-51d2-4c8a-8e31-e2e402d81010"
-  vsc_subnet_id        = "5af88a7e-b5f1-4f71-8716-0533b555d619"
-  access_key           = "ssh-rsa AAAAB3Nz_Lxyy1Q__ root_charon"
+  floating_ip_id       = "_FLOATING_IP_ID_"
+  vsc_floating_ip      = "_VSC_FLOATING_IP_"
+  vm_network_id        = "_VM_NETWORK_ID_"
+  vm_subnet_id         = "_VM_SUBNET_ID_"
+  vsc_network_id       = "_VSC_NETWORK_ID_"
+  vsc_subnet_id        = "_VSC_SUBNET_ID_
+  access_key           = "_ACCESS_KEY_"
   image_name           = "CentOS-8-stream"
   flavor_name          = "CPUv1.small"
-  ssh_forwarded_port   = "52203"
+  ssh_forwarded_port   = "_SSH_FORWARDED_PORT3_"
 }
 
 module "vm_with_pf_rules_with_ssh_access_with_nfs_share" {
   source   = "../modules/vm_with_pf_rules_with_ssh_access_with_nfs_share"
 
   vm_name              = "MyVM"
-  floating_ip_id       = "dd8b33b1-d178-4229-9df6-00157c61181c"
-  vm_network_id        = "985df437-2555-471d-9836-48355e6e7e9b"
-  vm_subnet_id         = "05f58ec4-5b6c-466a-9f18-78aac2ccf9f6"
-  nfs_network_id       = "2bab49b1-c5d1-48be-a670-911c46db53ad"
-  nfs_subnet_id        = "5c6b72d5-5245-4406-b52f-0b347364ac50"
-  access_key           = "ssh-rsa AAAAB3Nz_Lxyy1Q__ root_charon"
+  floating_ip_id       = "_FLOATING_IP_ID_"
+  vm_network_id        = "_VM_NETWORK_ID_"
+  vm_subnet_id         = "_VM_SUBNET_ID_"
+  nfs_network_id       = "_NFS_NETWORK_ID_"
+  nfs_subnet_id        = "_NFS_SUBNET_ID_"
+  access_key           = "_ACCESS_KEY_"
   image_name           = "CentOS-8-stream"
   flavor_name          = "CPUv1.small"
-  ssh_forwarded_port   = "52204"
+  ssh_forwarded_port   = "_SSH_FORWARDED_PORT4_"
   share_name           = "MyShare"
   share_size           = "10"
 }
-
---
+++ terraform/scripts/modify_variable.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+vm_floating_ip_cidr="193.190.80.0/25"
+vsc_floating_ip_cidr="172.24.48.0/20"
+
+source ../environment/app-cred-*-openrc.sh 
+retcode=$?
+if [ $retcode -ne 0 ]
+then
+	echo "Unable to source ../environment/app-cred-*-openrc.sh file." 1>&2
+	exit 1
+fi
+
+openstack catalog list &>/dev/null
+[ $? -ne 0 ] && echo "Unable to list openstack catalog. Exiting.." 1>&2 && exit 1
+
+vm_network_id="$(openstack network list -f value -c ID -c Name|grep '_vm'|cut -d ' ' -f1)" && \
+	echo "VM network id: $vm_network_id."
+vm_subnet_id="$(openstack network list -c Subnets -c Name|grep '_vm'|awk '{print $4}')" && \
+	echo "VM subnet id: $vm_subnet_id."
+nfs_network_id="$(openstack network list -f value -c ID -c Name|grep '_nfs'|cut -d ' ' -f1)" && \
+	echo "NFS network id: $nfs_network_id."
+nfs_subnet_id="$(openstack network list -c Subnets -c Name|grep '_nfs'|awk '{print $4}')" && \
+	echo "NFS subnet id: $nfs_subnet_id."
+vsc_network_id="$(openstack network list -f value -c ID -c Name|grep '_vsc'|cut -d ' ' -f1)" && \
+	echo "VSC network id: $vsc_network_id."
+vsc_subnet_id="$(openstack network list -c Subnets -c Name|grep '_vsc'|awk '{print $4}')" && \
+	echo "VSC subnet id: $vsc_subnet_id."
+
+access_key="$(openstack key list -c Name -f value|head -1)"
+[ -z "$access_key" ] && echo "Unable to find ssh access key. Exiting.." 1>&2 && exit 1
+echo "Using first ssh access key \"$access_key\"."
+
+ssh_forwarded_port1="$(shuf -i 51001-59999 -n 1)"
+ssh_forwarded_port2="$(shuf -i 51001-59999 -n 1)"
+ssh_forwarded_port3="$(shuf -i 51001-59999 -n 1)"
+ssh_forwarded_port4="$(shuf -i 51001-59999 -n 1)"
+http_forwarded_port="$(shuf -i 51001-59999 -n 1)"
+echo "Using ssh forwarded ports: $ssh_forwarded_port1 $ssh_forwarded_port2 $ssh_forwarded_port3 $ssh_forwarded_port4."
+echo "Using http forwarded port: $http_forwarded_port."
+
+while read line
+do
+	ip="$(echo \"$line\"|awk '{print $2}')"
+	ip_id="$(echo \"$line\"|awk '{print $1}')"
+	python3 -c "import ipaddress; ip = ipaddress.ip_address('$(echo \"$ip\")') in ipaddress.ip_network('$(echo \"$vm_floating_ip_cidr\")'); print (ip);"|grep "True" &>/dev/null && export floating_ip_id="$ip_id" && break
+done < <(openstack floating ip list -f value -c "Floating IP Address" -c ID -c "Port"|grep None)
+#[ -z "$floating_ip_id" ] && echo "Unable to find floating ip address. Exiting.." 1>&2 && exit 1
+echo "Using floating ip id: $floating_ip_id."
+while read line
+do
+        ip="$(echo \"$line\"|awk '{print $1}')"
+	python3 -c "import ipaddress; ip = ipaddress.ip_address('$(echo \"$ip\")') in ipaddress.ip_network('$(echo \"$vsc_floating_ip_cidr\")'); print (ip);"|grep "True" &>/dev/null && export vsc_floating_ip="$ip" && break
+done < <(openstack floating ip list -f value -c "Floating IP Address" -c "Port"|grep None)
+[ -z "$vsc_floating_ip" ] && echo "Unable to find VSC floating ip address. Exiting.." 1>&2 && exit 1
+echo "Using VSC floating ip: $vsc_floating_ip."
+
+
+sed -i "s/_VM_NETWORK_ID_/$vm_network_id/g" ../environment/main.tf
+sed -i "s/_VM_SUBNET_ID_/$vm_subnet_id/g" ../environment/main.tf
+sed -i "s/_NFS_NETWORK_ID_/$nfs_network_id/g" ../environment/main.tf
+sed -i "s/_NFS_SUBNET_ID_/$nfs_subnet_id/g" ../environment/main.tf
+sed -i "s/_VSC_NETWORK_ID_/$vsc_network_id/g" ../environment/main.tf
+sed -i "s/_VSC_SUBNET_ID_/$vsc_subnet_id/g" ../environment/main.tf
+sed -i "s/_ACCESS_KEY_/$access_key/g" ../environment/main.tf
+sed -i "s/_SSH_FORWARDED_PORT1_/$ssh_forwarded_port1/g" ../environment/main.tf
+sed -i "s/_SSH_FORWARDED_PORT2_/$ssh_forwarded_port2/g" ../environment/main.tf
+sed -i "s/_SSH_FORWARDED_PORT3_/$ssh_forwarded_port3/g" ../environment/main.tf
+sed -i "s/_SSH_FORWARDED_PORT4_/$ssh_forwarded_port4/g" ../environment/main.tf
+sed -i "s/_HTTP_FORWARDED_PORT_/$http_forwarded_port/g" ../environment/main.tf
+sed -i "s/_FLOATING_IP_ID_/$floating_ip_id/g" ../environment/main.tf
+sed -i "s/_VSC_FLOATING_IP_/$vsc_floating_ip/g" ../environment/main.tf
