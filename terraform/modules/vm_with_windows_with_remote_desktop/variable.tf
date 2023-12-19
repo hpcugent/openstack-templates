83ba833477ec26e34d45a094391d87bbc15e6fbc
Álvaro Simón <varosim@gmail.com>
2023-12-19 15:23:13 +0100

    Add windows VM example

---
+++ terraform/modules/vm_with_windows_with_remote_desktop/variable.tf
@@ -0,0 +1,20 @@
+variable "floating_ip_id" {
+}
+variable "vm_network_id" {
+}
+variable "vm_subnet_id" {
+}
+variable "vm_name" {
+}
+variable "access_key" {
+}
+variable "image_windows_id" {
+}
+variable "flavor_name" {
+}
+variable "ssh_forwarded_port" {
+}
+variable "root_fs_volume_size" {
+}
+variable "windows_admin_password" {
+}
--- terraform/scripts/modify_variable.sh
+++ terraform/scripts/modify_variable.sh
@@ -24,9 +24,6 @@ while getopts ":hk" option; do
    esac
 done
 
-#script logging to modify_variable.log file
-#test x$1 = x$'\x00' && shift || { set -o pipefail ; ( exec 2>&1 ; $0 $'\x00' "$@" ) | tee -a modify_variable.log ; exit $? ; }
-
 source modify_variable.config &>/dev/null
 [ -z ${IMAGE_NAME+x} ] && echo "Variable IMAGE_NAME is not set. Exiting.." 1>&2 && exit 1
 [ -z ${FLAVOR_NAME+x} ] && echo "Variable FLAVOR_NAME is not set. Exiting.." 1>&2 && exit 1
@@ -51,6 +48,9 @@ fi
 image_id="$(openstack image show "$IMAGE_NAME" -c id -f value)"
 	echo "Image id: $image_id. (Image name: $IMAGE_NAME)"
 	echo "Flavor name: $FLAVOR_NAME."
+image_windows_id="$(openstack image show "$IMAGE_WINDOWS_NAME" -c id -f value)"
+	echo "Image id: $image_windows_id. (Image name: $IMAGE_WINDOWS_NAME)"
+	echo "Flavor name: $FLAVOR_NAME."
 root_fs_volume_size=$(openstack flavor show "${FLAVOR_NAME}" -f value -c disk)
 	echo "Root FS volume size based on flavor disk size: $root_fs_volume_size."
 vm_network_id="$(openstack network list -f value -c ID -c Name|grep '_vm'|cut -d ' ' -f1)" && \
@@ -65,6 +65,7 @@ vsc_network_id="$(openstack network list -f value -c ID -c Name|grep '_vsc'|cut 
 	echo "VSC network id: $vsc_network_id."
 vsc_subnet_id="$(openstack network list -c Subnets -c Name|grep '_vsc'|awk '{print $4}')" && \
 	echo "VSC subnet id: $vsc_subnet_id."
+windows_admin_password="$(strings /dev/urandom | grep -o '[[:alnum:]]' | head -n 16 | tr -d '\n')"
 
 access_key="$(openstack keypair list -c Name -f value|head -1)"
 [ -z "$access_key" ] && echo "Unable to find ssh access key. Exiting.." 1>&2 && exit 1
@@ -94,7 +95,7 @@ generate_new_free_port () {
   allocated_ports=$(openstack floating ip port forwarding list "$floating_ip_id" -f value -c "External Port"|sort|uniq)
   for i in $(seq 100); do
     port="$(shuf -i 51001-59999 -n 1)"
-    if ! eval echo "$allocated_ports"|grep "$port" &>/dev/null; then
+    if [[ ! " $allocated_ports " =~ " $port " ]]; then
       new_port="$port"
       break
     fi
@@ -128,6 +129,7 @@ sed -i "s/_SHARE_SIZE_/$SHARE_SIZE/g" ../environment/main.tf
 sed -i "s/_VM_BASE_NAME_/$VM_BASE_NAME/g" ../environment/main.tf
 sed -i "s/_ROOT_FS_VOLUME_SIZE_/$root_fs_volume_size/g" ../environment/main.tf
 sed -i "s/_IMAGE_ID_/$image_id/g" ../environment/main.tf
+sed -i "s/_IMAGE_WINDOWS_ID_/$image_windows_id/g" ../environment/main.tf
 sed -i "s/_VM_NETWORK_ID_/$vm_network_id/g" ../environment/main.tf
 sed -i "s/_VM_SUBNET_ID_/$vm_subnet_id/g" ../environment/main.tf
 sed -i "s/_NFS_NETWORK_ID_/$nfs_network_id/g" ../environment/main.tf
@@ -135,11 +137,11 @@ sed -i "s/_NFS_SUBNET_ID_/$nfs_subnet_id/g" ../environment/main.tf
 sed -i "s/_VSC_NETWORK_ID_/$vsc_network_id/g" ../environment/main.tf
 sed -i "s/_VSC_SUBNET_ID_/$vsc_subnet_id/g" ../environment/main.tf
 sed -i "s/_ACCESS_KEY_/$access_key/g" ../environment/main.tf
-
+sed -i "s/_WINDOWS_ADMIN_PASSWORD_/$windows_admin_password/g" ../environment/main.tf
 
 if [[ "$KEEP_PF_PORTS" = 'false' ]]; then
   truncate -s 0 ../environment/used_ports.out
-  for suffix in $(seq 1 4); do
+  for suffix in $(seq 1 5); do
     generate_new_free_port
     declare ssh_forwarded_port"$suffix"="$new_port"
   done
@@ -149,7 +151,7 @@ else
   grep ssh_forwarded_port ../environment/main.tf
   index=1
   while IFS= read -r line; do
-    if [[ "$index" -eq 5 ]]; then
+    if [[ "$index" -eq 6 ]]; then
       declare http_forwarded_port="$line"
     else
       declare ssh_forwarded_port$index="$line"
@@ -160,8 +162,9 @@ fi
 
 echo "Using ssh forwarded ports: $ssh_forwarded_port1 $ssh_forwarded_port2 $ssh_forwarded_port3 $ssh_forwarded_port4."
 echo "Using http forwarded port: $http_forwarded_port."
+echo "Using rdp forwarded port: $ssh_forwarded_port5."
 
-for suffix in $(seq 1 4); do
+for suffix in $(seq 1 5); do
   port=ssh_forwarded_port${suffix}
   sed -i "s/_SSH_FORWARDED_PORT${suffix}_/${!port}/g" ../environment/main.tf
   echo "${!port}" >> ../environment/used_ports.out
@@ -182,4 +185,5 @@ echo "(${VM_BASE_NAME})           ssh -p $ssh_forwarded_port1 <user>@$floating_i
 echo "(${VM_BASE_NAME}-nginx)     ssh -p $ssh_forwarded_port2 <user>@$floating_ip"
 echo "(${VM_BASE_NAME}-vsc_net)   ssh -p $ssh_forwarded_port3 <user>@$floating_ip"
 echo "(${VM_BASE_NAME}-nfs_share) ssh -p $ssh_forwarded_port4 <user>@$floating_ip"
+echo "(${VM_BASE_NAME}-windows)   rdesktop $floating_ip:$ssh_forwarded_port5"
 echo
