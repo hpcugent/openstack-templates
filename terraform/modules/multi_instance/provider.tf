bfe9febe59e0040bcfc6875e690952de45314127
GoodOldJack12 <deloght.jonathan@gmail.com>
2024-09-13 10:23:05 +0200

    Revert "Update terraform provider"
    This reverts commit ca1da602b97ecf90fdb8e8a2c32d54b1b64dadca.

--- terraform/modules/multi_instance/provider.tf
+++ terraform/modules/multi_instance/provider.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     openstack = {
       source  = "terraform-provider-openstack/openstack"
-      version = "2.1.0"
+      version = "2.0.0"
     }
     random = {
       source  = "hashicorp/random"
