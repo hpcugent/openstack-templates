edeb07cc77e389613a64be93009a5ac014059505
GoodOldJack12 <deloght.jonathan@gmail.com>
2024-10-02 13:22:01 +0200

    Fix nfs network name

--- terraform/modules/nfs/data.tf
+++ terraform/modules/nfs/data.tf
@@ -1,5 +1,5 @@
 data "openstack_networking_network_v2" "nfs" {
-  name = "${var.project_name}_nfs"
+  name = "${var.project_name}_nfs_vxlan"
 }
 data "openstack_networking_subnet_ids_v2" "nfs" {
   network_id = data.openstack_networking_network_v2.nfs.id
